069022f3041176c3397bc643ee1c24c7a3e8c622
abhisheknigam5714 <an5714170@gmail.com>
2026-08-11 17:44:33 +0530

    day2 terrform

---
+++ day2/variable.tf
@@ -0,0 +1,16 @@
+variable "ami" {
+  description = "ami configuration"
+  type        = string
+
+}
+
+variable "type" {
+  description = "instance type configuration"
+  type        = string
+
+}
+variable "key_name" {
+  description = "key name configuration"
+  type        = string
+
+}
